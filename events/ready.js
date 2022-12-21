ad7a69ef86bca4b6de564378018664fc6b723206
Noah DePalma <noahgdepalma@gmail.com>
2022-12-21 12:30:25 -0500

    added custom status

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,7 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
+        client.user.setActivity('your cities grow | /invite', { type: 'WATCHING' });
         console.log(`Logged in as ${client.user.tag}!`);
     },
 };
