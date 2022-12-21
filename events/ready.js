5981e5c308b18c88febe3a96cbaff563bc7c80c4
Noah DePalma <noahgdepalma@gmail.com>
2022-12-21 12:36:28 -0500

    fixed status

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        client.user.setActivity('your cities grow | /invite', { type: 'WATCHING' });
+        client.user.setActivity('your cities grow | /invite', { type: ActivityType.Watching })
         console.log(`Logged in as ${client.user.tag}!`);
     },
 };
