981e3598593ee44d81e87d530feafc616c86f026
Noah DePalma <noahgdepalma@gmail.com>
2023-01-28 12:36:05 -0500

    added /vote!!!

---
+++ commands/vote.js
@@ -0,0 +1,46 @@
+const { Cities } = require('../models/cities.js');
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('vote')
+        .setDescription('Vote for UrbanBot on Top.gg!')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('link')
+                .setDescription('Vote for UrbanBot on Top.gg!')    
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('claim')
+                .setDescription('Claim your reward for voting on Top.gg!')
+        ),
+    async execute(interaction) {
+        const cityExists = await Cities.findByPk(interaction.user.id);
+        const cooldown = interaction.client.voteCooldown;
+
+        if (interaction.options.getSubcommand() === 'link') {
+            return interaction.reply('Vote for UrbanBot here: https://top.gg/bot/1025198176547909693/vote');
+        } else if (interaction.options.getSubcommand() === 'claim') {
+            if (cityExists) {
+                if (!cooldown.has(interaction.user.id)) return interaction.reply('You haven\'t voted yet! Vote for UrbanBot here: https://top.gg/bot/1025198176547909693/vote');
+
+                const cityBalance = await Cities.findOne({
+                    attributes: ['userId', 'balance', 'resources'],
+                    where: { userId: interaction.user.id },
+                });
+
+                await cityBalance.increment({
+                    'balance': 10000,
+                    'resources': 9000,
+                });
+
+                await interaction.reply('Thank you for voting! You claimed your reward of **$10,000** and **9,000** resources!');
+
+                cooldown.delete(interaction.user.id);
+            } else {
+                return interaction.reply('You haven\'t created a city! Use `/found` to create one!');
+            }
+        }
+    }
+}
--- index.js
+++ index.js
@@ -3,17 +3,32 @@ const path = require('node:path');
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { AutoPoster } = require('topgg-autoposter');
 const { token, topggToken } = require('./config.json');
+const { Webhook } = require('@top-gg/sdk');
+const app = require('express')();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 client.collectCooldown = new Collection();
 client.dailyCooldown = new Collection();
+client.voteCooldown = new Collection();
 
 if (topggToken !== 'TEST') {
     const ap = AutoPoster(topggToken, client);
 
     ap.on('posted', (stats) => {
         console.log(`Successfully posted bot stats to Top.gg, UrbanBot is on ${stats.serverCount} servers.`);
+    });
+
+    const dblWebhook = new Webhook(topggToken);
+
+    app.post('/dblwebhook', dblWebhook.listener(vote => {
+        console.log(`User voted on Top.gg! (id: ${vote.user})`);
+
+        client.voteCooldown.set(vote.user, true);
+    }));
+
+    app.listen(8087, () => {
+        console.log('Top.gg vote webhook is listening on port 8087!');
     });
 }
 
